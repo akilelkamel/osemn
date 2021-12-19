78eb60cce5a9eeef87e12be4d417319f806751ee
Akil Elkamel <akil_elkamel@yahoo.fr>
2021-12-19 23:09:28 +0300

    All steps with obtain only

--- R/obtain.R
+++ R/obtain.R
@@ -1,11 +1,45 @@
+#' @title "obtain" data from various data sources
+#'
+#' @description It is the first step in the `OSEMN` data science pipeline. It allows to read data from a chosen data source
+#'
+#' @param from the type of data source from which the data will be obtained.
+#' This should be one of "csv", "tsv", "excel", "rds", "mysql".
+#' @param ... any data source specific parameter
+#'
+#' @return a data frame containg the data
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' #read data for a csv file
+#' my_data <- obtain(from = "csv", file = "data.csv")
+#'
+#' #read data for a csv file
+#' my_data <- obtain(from = "csv", file = "data.csv")
+#'
+#' #read data for a csv file
+#' my_data <- obtain(from = "csv", file = "data.csv")
+#'
+#' #read data for a csv file
+#' my_data <- obtain(from = "csv", file = "data.csv")
+#' }
+#'
 obtain <- function(from = "csv", ...){
-  supported_sources <- c("csv", "tsv", "excel", "dat", "mysql")
+  supported_sources <- c("csv", "tsv", "excel", "rds", "mysql")
   from = stringr::str_to_lower(from)
   if(!from %in% supported_sources){
     stop(paste0("`from` must be one of the following supported source formats: [", paste(as.character(supported_sources), collapse=", "), "]"))
   } else {
     if(from == "csv"){
       df <- readr::read_csv(...)
+    } else if (from == "tsv"){
+      df <- readr::read_tsv(...)
+    } else if (from == "excel"){
+
+    } else if (from == "rds"){
+      df <- readRDS(...)
+    } else if (from == "mysql"){
+
     }
   }
   return(df)
